d144dd23fff6acc994be588f764930b91cd0db51
bgoosman <bgoosman@gmail.com>
2025-02-03 17:59:53 -0500

    fix publish

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule KuzuPyPortEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:export, "~> 0.1.0"}
     ]
   end
